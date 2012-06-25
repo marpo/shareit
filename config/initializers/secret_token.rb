9c72c9d27f52a63b6e365124a4959efc84b4c3a5
Marcel Portillo <marc.porto@hotmail.com>
2012-06-25 16:27:14 +0200

    rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shareit::Application.config.secret_token = '99a8d9e90ef83cf4d391382152e25c0fbf0262ec05560706fe606b837c721f05e68e600dd44bb22c33e77dc44586fa5303250b00f1ba4b1201833f1a8af01674'
