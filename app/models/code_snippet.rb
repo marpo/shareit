44ac37b5903f85db6aaddbceb7759e6c3fc7a155
Marcel Portillo <marc.porto@hotmail.com>
2012-06-28 13:41:50 +0200

    Styling der Inputfelder angepasst und CodeSnippet Model kompletiert

--- app/models/code_snippet.rb
+++ app/models/code_snippet.rb
@@ -1,5 +1,8 @@
 class CodeSnippet < ActiveRecord::Base
-  attr_accessible :beschreibung, :code, :sprache, :titel, SPRACHE
-  validates :sprache, inclusion: SPRACHE
+
   SPRACHE = [ "Ruby", "C", "Html", "etc." ]
+
+  attr_accessible :beschreibung, :code, :sprache, :titel
+  validates :sprache, inclusion: SPRACHE
+  validates :titel, :code, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120627160324) do
+ActiveRecord::Schema.define(:version => 20120627210450) do
+
+  create_table "code_snippets", :force => true do |t|
+    t.string   "titel"
+    t.string   "sprache"
+    t.text     "beschreibung"
+    t.text     "code"
+    t.string   "autor"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "links", :force => true do |t|
     t.string   "url"
