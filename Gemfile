0f94b24b919d0615344086af73f3fba51ddd3f58
Marcel Portillo <marc.porto@hotmail.com>
2012-06-29 11:17:17 +0200

    Codehighliting

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jquery-rails'
 
 gem 'bcrypt-ruby'
 
+gem 'coderay'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def codeAnfaerben(code_snippet)
+		CodeRay.scan(code_snippet, :ruby).div(:line_numbers => :table).html_safe
+	end
 end
